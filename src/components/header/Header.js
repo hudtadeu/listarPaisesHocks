5275f1181fca94f3117d65555f2207416968ae52
Hudson Tadeu <hudinvesting@gmail.com>
2022-11-03 19:08:14 -0300

    Initial commit

---
+++ src/components/header/Header.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { formatNumber } from '../helpers/formatHelpers';
+
+import css from './header.module.css';
+
+export default function Header({ filter, countCountry, totalPopulation, onChangeFilter }) {
+  const handleInputChange = (event) => {
+    const newText = event.target.value;
+    onChangeFilter(newText);
+  };
+
+  return (
+    <div className={css.flexRow}>
+      <input
+        placeholder='Filtro'
+        type="text"
+        value={filter}
+        onChange={handleInputChange}
+      />
+      <span className={css.countries}>
+        Países: <strong>{countCountry}</strong>{' '}
+      </span>{' '}
+      <span className={css.population}>
+        População: <strong>{formatNumber(totalPopulation)}</strong>{' '}
+      </span>{' '}
+    </div>
+  )
+}
